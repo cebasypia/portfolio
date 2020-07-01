a7584fdb0ee6fe97a4e2b35af03769d2ea2ced8f
cebasypia <cebasypia.tskm8@gmail.com>
2020-07-01 07:02:25 +0000

    Add material design icons

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import App from '../app.vue'
 import router from './router.js'
 
