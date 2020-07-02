2da758c4d1b067d33039c29d929d3e32123933c5
cebasypia <cebasypia.tskm8@gmail.com>
2020-07-02 02:56:28 +0000

    Fix by linter

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,19 +1,20 @@
 <template>
   <v-app>
-  <AppBar></AppBar>
-    <router-view></router-view>
+    <AppBar></AppBar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import AppBar from './packs/components/appBar.vue'
+import AppBar from "./packs/components/appBar.vue";
 
 export default {
-  components : {
-    AppBar
-  }
-}
+  components: {
+    AppBar,
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
