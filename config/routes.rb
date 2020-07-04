d35e2e3feba0d72570b310704924a2c3bae85093
cebasypia <cebasypia.tskm8@gmail.com>
2020-07-04 12:55:49 +0000

    Add auth pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   # APIコントローラへのルーティング
   namespace :api, { format: 'json' } do
     namespace :v1 do
-      resources :users, only: [:index, :show]
+      resources :users, only: [:show, :create, :update, :destroy]
+      post   '/login',   to: 'sessions#create'
+      delete '/logout',  to: 'sessions#destroy'
+      get '/logged_in',  to: 'sessions#logged_in'
       post '/tweets', controller: 'tweets', action: 'search'
       get '/tweets/user/:id', controller: 'tweets', action: 'user'
       get '/tweets/:id', controller: 'tweets', action: 'show'
-      # post  '/users', controller: 'users/registrations', action: 'create'
     end
   end
 end
