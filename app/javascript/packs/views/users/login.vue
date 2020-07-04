bd1f061be7bc026a201b8626247a2850cd200b5b
cebasypia <cebasypia.tskm8@gmail.com>
2020-07-04 12:53:45 +0000

    Add login page

---
+++ app/javascript/packs/views/users/login.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <v-alert
+      v-if="errors.length != 0"
+      border="bottom"
+      colored-border
+      type="warning"
+      elevation="2"
+    >
+      <ul v-for="error in errors" :key="error">
+        <li>
+          <font color="red">{{ error }}</font>
+        </li>
+      </ul>
+    </v-alert>
+    <v-text-field
+      v-model="session.email"
+      :rules="rules.email"
+      label="E-mail"
+      required
+    ></v-text-field>
+    <v-text-field
+      v-model="session.password"
+      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+      :rules="rules.password"
+      :type="show1 ? 'text' : 'password'"
+      label="Password"
+      counter
+      @click:append="show1 = !show1"
+    ></v-text-field>
+    <v-btn :disabled="!valid" color="success" class="mr-4" @click="login"
+      >Log in</v-btn
+    >
+  </v-form>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data: function () {
+    return {
+      session: {
+        email: '',
+        password: '',
+      },
+      errors: '',
+      valid: true,
+      show1: false,
+      rules: {
+        email: [
+          (v) => !!v || 'E-mail is required',
+          (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        ],
+        password: [(v) => v.length >= 6 || 'Min 6 characters'],
+      },
+    }
+  },
+  methods: {
+    login: function () {
+      axios
+        .post('/api/v1/login', this.session)
+        .then((response) => {
+          let user = response.data
+          this.$router.push({ name: 'UsersShowPage', params: { id: user.id } })
+        })
+        .catch((error) => {
+          console.error(error)
+          if (error.response.data && error.response.data.errors) {
+            this.errors = error.response.data.errors
+          }
+        })
+    },
+  },
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  list-style: none;
+}
+</style>
