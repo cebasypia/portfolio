e01facff85e5ebf38a49c47d97e56bb35ee7abf4
cebasypia <cebasypia.tskm8@gmail.com>
2020-07-04 12:51:37 +0000

    Add data: current_user

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <AppBar></AppBar>
+    <AppBar :current_user="current_user"></AppBar>
     <v-main>
       <router-view></router-view>
     </v-main>
@@ -11,11 +11,31 @@
 <script>
 import AppBar from './packs/components/appBar.vue'
 import ToolBar from './packs/components/toolBar.vue'
+import axios from 'axios'
 
 export default {
+  data: () => {
+    return {
+      current_user: null,
+    }
+  },
+  mounted() {
+    axios
+      .get('/api/v1/logged_in')
+      .then((response) => (this.current_user = response.data))
+  },
   components: {
     AppBar,
     ToolBar,
+  },
+  watch: {
+    $route: function (to, from) {
+      if (to.path !== from.path) {
+        axios
+          .get('/api/v1/logged_in')
+          .then((response) => (this.current_user = response.data))
+      }
+    },
   },
 }
 </script>
