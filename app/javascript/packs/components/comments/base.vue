9b8989a95d4b6b2c3f02f9b58ffd15bc6c8c9cb7
cebasypia <cebasypia.tskm8@gmail.com>
2020-07-07 11:10:25 +0000

    Add comment components

---
+++ app/javascript/packs/components/comments/base.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <Card
+      v-for="comment in comments"
+      :comment="comment"
+      :key="comment.id"
+      @update="update"
+    ></Card>
+    <Form id="comment_form" @update="update"></Form>
+  </div>
+</template>
+
+<script>
+import Form from './form'
+import Card from './card'
+import axios from 'axios'
+export default {
+  data: function () {
+    return {
+      comments: [],
+      dialog: false,
+    }
+  },
+  mounted() {
+    axios
+      .get(`/api/v1/tweets/${this.$route.params.id}/comments`)
+      .then((response) => (this.comments = response.data))
+  },
+  methods: {
+    update() {
+      axios
+        .get(`/api/v1/tweets/${this.$route.params.id}/comments`)
+        .then((response) => {
+          this.comments = response.data
+          this.dialog = false
+        })
+    },
+  },
+  components: {
+    Form,
+    Card,
+  },
+}
+</script>
+
+<style scoped>
+#comment_form {
+  position: fixed;
+  bottom: 56px;
+  left: 0;
+  width: 100%;
+  background-color: lightgray;
+}
+</style>
