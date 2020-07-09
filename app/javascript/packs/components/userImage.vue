dad6d380b900b149924113bbb1fa8db37c1b7864
cebasypia <cebasypia.tskm8@gmail.com>
2020-07-09 09:20:45 +0000

    Create User Image

---
+++ app/javascript/packs/components/userImage.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-list-item-avatar
+    :size="size"
+    color="grey"
+    @click="linkTo('UsersShowPage', { id: user.id })"
+  >
+    <v-img :src="user.image_url"></v-img>
+  </v-list-item-avatar>
+</template>
+
+<script>
+import router from '../router.js'
+
+export default {
+  props: {
+    size: {
+      type: Number,
+      default: 73,
+    },
+    user: Object,
+    is_link: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  methods: {
+    linkTo(name, params) {
+      if (this.is_link) {
+        router.push({ name: name, params: params })
+      }
+    },
+  },
+}
+</script>
