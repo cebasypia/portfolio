79e388f3d93822139d5ec083d1647ecfab8a12b5
cebasypia <cebasypia.tskm8@gmail.com>
2020-07-09 07:56:05 +0000

    Add image_url

--- db/migrate/20200704011621_create_users.rb
+++ db/migrate/20200704011621_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :image_url
       t.string :profile
       t.string :password_digest
 
