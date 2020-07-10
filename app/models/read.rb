42381069bb646532f977de3753bfb3ae5bffcbb9
cebasypia <cebasypia.tskm8@gmail.com>
2020-07-10 07:32:12 +0000

    Move converting to json

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,6 +1,18 @@
+require 'action_view'
+require 'action_view/helpers'
+
 class Read < ApplicationRecord
   include TwitterApi
+  include ActionView::Helpers::DateHelper
 
   belongs_to :user
   validates :user_id, uniqueness: { scope: :tweet_id }
+
+  def json
+    reads = attributes
+    reads["created_at"] = time_ago_in_words created_at
+    reads["user"] = user.json
+    reads["tweet"] = tweet # self.tweet
+    reads
+  end
 end
