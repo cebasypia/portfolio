8e5b172fdb291ea679370a8ce86c82d30fd25053
cebasypia <cebasypia.tskm8@gmail.com>
2020-07-10 05:47:30 +0000

    Add Read Card

---
+++ app/javascript/packs/components/reads/card.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card class="align-center mx-auto">
+    <v-list-item two-line>
+      <UserImage :size="73" :user="read.user"></UserImage>
+      <v-list-item-content>
+        <v-list-item-title>
+          {{ read.user.name }}
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-card-text> {{ read.created_at }} によんだ！ </v-card-text>
+    <div class="tweet-card-container">
+      <TweetCardMini class="tweet-card" :tweet="read.tweet"></TweetCardMini>
+    </div>
+  </v-card>
+</template>
+<script>
+import UserImage from '../userImage.vue'
+import TweetCardMini from '../tweetCardMini.vue'
+
+export default {
+  props: ['read'],
+  data: function () {
+    return {}
+  },
+  methods: {},
+  components: {
+    UserImage,
+    TweetCardMini,
+  },
+}
+</script>
+
+<style scoped>
+.tweet-card {
+  box-shadow: initial !important;
+  border-color: rgba(0, 172, 237, 1);
+  border-style: solid;
+}
+.tweet-card-container {
+  padding: 1rem;
+}
+</style>
