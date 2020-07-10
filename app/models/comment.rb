fd390619fee5d6b8648d724069717d7c589c691c
cebasypia <cebasypia.tskm8@gmail.com>
2020-07-10 05:45:16 +0000

    Add time_ago_in_words

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
+require 'action_view'
+require 'action_view/helpers'
+
 class Comment < ApplicationRecord
   include TwitterApi
+  include ActionView::Helpers::DateHelper
   belongs_to :user
 
   validates :content, presence: true, length: { maximum: 140 }
@@ -8,6 +12,7 @@ class Comment < ApplicationRecord
     comment = {}
     comment["id"] = id
     comment["content"] = content
+    comment["created_at"] = time_ago_in_words created_at
     comment["user"] = user
     comment["tweet"] = tweet # self.tweet
     comment
